dd012ec44d2564babd4c7a2435fdcf8f94bb961b
Fernando Torterolo <nandotorterolo@gmail.com>
2024-10-15 17:24:36 -0300

    fix docker image on regtest sh

--- setupBifrostRegtest.sh
+++ setupBifrostRegtest.sh
@@ -18,7 +18,7 @@ bifrost:
 "
 chmod 777 node01/config.yaml
 echo $(pwd)
-export CONTAINER_ID=$(docker run -d --name bifrost01 -p 9185:9085 -p 9184:9084 -p 9191:9091 -v $(pwd)/node01:/bifrost-staking:rw ghcr.io/topl/bifrost-node:2.0.0-beta3-24-7fd725a9 --  --config=/bifrost-staking/config.yaml --regtest)
+export CONTAINER_ID=$(docker run -d --name bifrost01 -p 9185:9085 -p 9184:9084 -p 9191:9091 -v $(pwd)/node01:/bifrost-staking:rw docker.io/stratalab/strata-node:0.0.0-8200-805081dd --  --config=/bifrost-staking/config.yaml --regtest)
 export IP_CONTAINER=$(docker network inspect bridge | jq  ".[0].Containers.\"$CONTAINER_ID\".IPv4Address" | sed  's:"::g' | sed -n 's:\(.*\)/.*:\1:p')
 echo "IP_CONTAINER: $IP_CONTAINER"
 echo > node02/config.yaml "\
@@ -33,4 +33,4 @@ bifrost:
     known-peers: $IP_CONTAINER:9085
 "
 chmod 777 node02/config.yaml
-docker run -d --name bifrost02 -p 9087:9085 -p 9086:9084 -p 9092:9091 -v $(pwd)/node02:/bifrost-staking ghcr.io/topl/bifrost-node:2.0.0-beta3-24-7fd725a9 --  --config  /bifrost-staking/config.yaml --regtest
+docker run -d --name bifrost02 -p 9087:9085 -p 9086:9084 -p 9092:9091 -v $(pwd)/node02:/bifrost-staking docker.io/stratalab/strata-node:0.0.0-8200-805081dd --  --config  /bifrost-staking/config.yaml --regtest
